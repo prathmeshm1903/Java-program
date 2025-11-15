33f7b672d8be35c91ae7139600497b34276a762f
Prathmesh Mali <prathmeshmali019@gmail.com>
2025-11-15 10:29:51 +0530

    Added multiply program

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 15;
+        int num2 = 6;
+        int product = num1 * num2;
+        System.out.println("The product is: " + product);
+    }
+}

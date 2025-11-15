b71d908359e18e170380e495f3a4b89be27e6080
Prathmesh Mali <prathmeshmali019@gmail.com>
2025-11-15 10:23:05 +0530

    Added add Java program

---
+++ add.java
@@ -0,0 +1,13 @@
+public class AdditionExample {
+    public static void main(String[] args) {
+        // Declare and initialize two integer variables
+        int number1 = 10;
+        int number2 = 25;
+
+        // Calculate the sum
+        int sum = number1 + number2;
+
+        // Print the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
